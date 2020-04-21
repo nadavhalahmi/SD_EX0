b3d48b91abed201c3e19c2cf543f7a577370ab77
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-04-21 16:35:28 +0300

    full parser. should handle 1 TODO and exceptions

--- library/src/main/kotlin/Parser.kt
+++ library/src/main/kotlin/Parser.kt
@@ -1,8 +1,16 @@
+//TODO: replace Any with union of dict, list, int, string
+
 class TorrentParser {
-    fun parseElement(torrent: ByteArray, i: Int, stopCond: (Int) -> Boolean): Pair<String, Int> {
+    /**
+     * [torrent]: the torrent to parse
+     * [startIndex]: the start index of the element
+     * [stopCond]: the condition for stop. depends on current index
+     * @return: element, element's length
+     */
+    private fun parseElement(torrent: ByteArray, startIndex: Int, stopCond: (Int) -> Boolean): Pair<String, Int> {
         var key = ""
         var len = 0
-        var index = i
+        var index = startIndex
         while(!stopCond(index)) {
             key += torrent[index].toChar()
             len++
@@ -11,7 +19,31 @@ class TorrentParser {
         return Pair(key, len)
     }
 
-    fun parseNumElemPair(torrent: ByteArray, index: Int): Pair<String, Int> {
+    private fun parseValue(torrent: ByteArray, startIndex: Int): Pair<Any, Int>{
+        val res: Pair<Any, Int>
+        when(torrent[startIndex].toChar()) {
+            in '0'..'9' -> {
+                res = parseNumElemPair(torrent, startIndex)
+            }
+            'i' -> {
+                res = parseInt(torrent, startIndex)
+            }
+            'l' -> {
+                res = parseList(torrent, startIndex)
+            }
+            'd' -> {
+                res = parseDict(torrent, startIndex)
+            }
+            else -> {
+                throw Exception("Invalid Torrent")
+            }
+        }
+        return Pair(res.first, res.second)
+    }
+
+
+    private fun parseNumElemPair(torrent: ByteArray, index: Int): Pair<String, Int> {
+        assert(torrent[index].toChar() in '1'..'9')
         var pairLen = 0
         var res: Pair<String, Int> = parseElement(torrent, index) {torrent[it].toChar() == ':'}
         val elemLen = res.first.toInt()
@@ -22,164 +54,62 @@ class TorrentParser {
         pairLen += res.second
         return Pair(elem, pairLen)
     }
+    private fun parseList(torrent: ByteArray, startIndex: Int): Pair<List<Any>, Int>{
+        assert(torrent[startIndex].toChar() == 'l')
+        val lst = ArrayList<Any>()
+        var index = startIndex+1 //pass 'l'
+        var res: Pair<Any, Int>
+        var len = 1 //for 'l'
+        while(torrent[index].toChar() != 'e'){
+            res = parseValue(torrent, index)
+            lst.add(res.first)
+            index += res.second
+            len += res.second
+        }
+        assert(torrent[index].toChar() == 'e')
+        len++ //pass 'e'
+        return Pair(lst, len)
+    }
 
-    fun parseDict(torrent: ByteArray, startIndex: Int): HashMap<String, String>{
-        val dict = HashMap<String, String>() //TODO: fix second type to Union of dict, list, int, string
-        var index = startIndex
-        var key: String = ""
-        var value: String = ""
-        var res: Pair<String, Int>
+    private fun parseDict(torrent: ByteArray, startIndex: Int): Pair<HashMap<String, Any>, Int>{
+        assert(torrent[startIndex].toChar() == 'd')
+        val dict = HashMap<String, Any>()
+        var index = startIndex+1
+        var key: String
+        var res: Pair<Any, Int>
+        var len = 1
         while(torrent[index].toChar() != 'e'){
             when(torrent[index].toChar()){
                 in '0'..'9' -> {
                     res = parseNumElemPair(torrent, index)
                     key = res.first
                     index += res.second
+                    len += res.second
                 }
-                //TODO: continue with other types
+                else -> throw Exception("DictKeyShouldBeString")
             }
-            when(torrent[index].toChar()) {
-                in '0'..'9' -> {
-                    res = parseNumElemPair(torrent, index)
-                    value = res.first
-                    index += res.second
-                }
-                //TODO: continue with other types
-            }
-            dict[key] = value
+            res = parseValue(torrent, index)
+            dict[key] = res.first
+            index += res.second
+            len += res.second
         }
-        return dict
+        assert(torrent[index].toChar() == 'e')
+        len++ //pass 'e'
+        return Pair(dict, len)
     }
 
 
-    fun parse(torrent: ByteArray): HashMap<String, String>{ //TODO: fix return type as above
+    private fun parseInt(torrent: ByteArray, startIndex: Int): Pair<Int, Int> {
+        assert(torrent[startIndex].toChar() == 'i')
+        val res = parseElement(torrent, startIndex+1) {torrent[it].toChar() == 'e'}
+        return Pair(res.first.toInt(), res.second+2) //+2 for i and e
+    }
+
+
+    fun parse(torrent: ByteArray): HashMap<String, Any>{
         when(torrent[0].toChar()){
-            'd' -> return parseDict(torrent, 1)
+            'd' -> return parseDict(torrent, 0).first
         }
         return hashMapOf()
     }
 }
-
-
-//package app.shynline.torient.torrent.bencoding
-//
-//import app.shynline.torient.torrent.bencoding.common.BItem
-//import app.shynline.torient.torrent.bencoding.common.Chars
-//import app.shynline.torient.torrent.bencoding.common.InvalidBencodedException
-//import java.io.BufferedInputStream
-//import java.io.InputStream
-//import java.util.*
-//
-//class BDict(bencoded: ByteArray? = null, item: LinkedHashMap<BString, BItem<*>>? = null) :
-//    BItem<LinkedHashMap<BString, BItem<*>>>(bencoded, item) {
-//
-//    override fun encode(): ByteArray {
-//        var res = "d".toByteArray()
-//        value().forEach {
-//            res += it.key.encode()
-//            res += it.value.encode()
-//        }
-//        res += "e".toByteArray()
-//        return res
-//    }
-//
-//    override fun decode(bencoded: ByteArray): LinkedHashMap<BString, BItem<*>> {
-//        var bc = bencoded.copyOf()
-//        if (bc[0] != Chars.d)
-//            throw InvalidBencodedException(
-//                "BDict literals should start with d and end with e."
-//            )
-//        bc = bc.copyOfRange(1, bc.size)
-//        val res: LinkedHashMap<BString, BItem<*>> = linkedMapOf()
-//        var index: Int
-//        var sub: ByteArray
-//        var key: BString
-//        var str: BString
-//        while (bc[0] != Chars.e) {
-//            if (!bc[0].toChar().isDigit())
-//                throw InvalidBencodedException(
-//                    "Invalid Bencoded Dict."
-//                )
-//            key = BString(bencoded = bc)
-//            bc = bc.copyOfRange(key.encode().size, bc.size)
-//            when (bc[0]) {
-//                Chars.i -> {
-//                    index = bc.indexOfFirst { it == Chars.e }
-//                    if (index == -1)
-//                        throw InvalidBencodedException(
-//                            "Invalid Bencoded Dict."
-//                        )
-//                    sub = bc.toList().subList(0, index + 1).toByteArray()
-//                    res[key] = BInteger(bencoded = sub)
-//                    bc = bc.copyOfRange(sub.size, bc.size)
-//                }
-//                Chars.l -> {
-//                    val bl = BList(bencoded = bc)
-//                    res[key] = bl
-//                    bc = bc.copyOfRange(bl.encode().size, bc.size)
-//                }
-//                Chars.d -> {
-//                    val bd = BDict(bencoded = bc)
-//                    res[key] = bd
-//                    bc = bc.copyOfRange(bd.encode().size, bc.size)
-//                }
-//                else -> {
-//                    if (!bc[0].toChar().isDigit())
-//                        throw InvalidBencodedException(
-//                            "Invalid Bencoded Dict."
-//                        )
-//                    str = BString(bencoded = bc)
-//                    res[key] = str
-//                    bc = bc.copyOfRange(str.encode().size, bc.size)
-//                }
-//            }
-//        }
-//        return res
-//    }
-//
-//    override fun toString(short: Boolean, n: Int): String {
-//        var mShort = short
-//        val size = value().size
-//        if (size < n + 1)
-//            mShort = false
-//        return buildString {
-//            append("{")
-//            value().toList().filterIndexed { index, pair ->
-//                !mShort || index == size - 1 || index < n - 1
-//            }.forEachIndexed { index, bItem ->
-//                append(" ")
-//                append(bItem.first.toString(mShort))
-//                append(":")
-//                append(bItem.second.toString(mShort))
-//                if (!mShort) {
-//                    if (index < size - 1)
-//                        append(",")
-//                } else {
-//                    if (index < n - 1)
-//                        append(",")
-//                    if (index == n - 2) {
-//                        append(" ... ")
-//                        append(",")
-//                    }
-//                }
-//            }
-//            append(" }")
-//        }
-//    }
-//
-//    operator fun get(key: String): BItem<*>? {
-//        return value()[BString(key)]
-//    }
-//
-//    fun containsKey(key: String): Boolean {
-//        return value().containsKey(BString(key))
-//    }
-//
-//    companion object {
-//        fun fromInputStream(inputStream: InputStream): BDict {
-//            return BDict(bencoded = BufferedInputStream(inputStream).use { s ->
-//                s.readBytes()
-//            })
-//        }
-//    }
-//}
--- library/src/test/kotlin/ParserTest.kt
+++ library/src/test/kotlin/ParserTest.kt
@@ -2,13 +2,58 @@ import org.junit.jupiter.api.Test
 
 class ParserTest {
     private val parser = TorrentParser()
-    private val simpleDict = "d3:cow3:moo4:spam4:eggse".toByteArray()
+    private val simpleDictTorrent = "d3:cow3:moo4:spam4:eggse"
+    private val simpleDictRes = mapOf<String, String>("cow" to "moo", "spam" to "eggs")
+    private val dictWithIntsTorrent = "d3:onei1e7:-twelvei-12ee"
+    private val dictWithIntsRes = mapOf<String, Int>("one" to 1, "-twelve" to -12)
+    private val dictOfDictsTorrent = ("d3:one"+simpleDictTorrent+ "3:two"+dictWithIntsTorrent+"e")
+    private val dictOfDictsRes = mapOf<String, Any>("one" to simpleDictRes, "two" to dictWithIntsRes)
+    private val dictWithListTorrent = "d4:spaml1:a1:bee"
+    private val dictWithListRes = mapOf<String, Any>("spam" to listOf("a", "b"))
+    private val emptyDictTorrent = "de"
+    private val emptyDictRes = mapOf<String, Any>()
+    private val dictWithEmptyListTorrent = "d3:onelee"
+    private val dictWithEmptyListRes = mapOf<String, Any>("one" to listOf<Any>())
+
+    @Test
+    fun `empty dict`() {
+        val dict = parser.parse(emptyDictTorrent.toByteArray())
+
+        assert(dict == emptyDictRes)
+    }
 
     @Test
     fun `full check simple strings dict`() {
-        val dict = parser.parse(simpleDict)
+        val dict = parser.parse(simpleDictTorrent.toByteArray())
 
-        assert(dict == mapOf<String, String>("cow" to "moo", "spam" to "eggs"))
+        assert(dict == simpleDictRes)
     }
 
+    @Test
+    fun `dict with int`() {
+        val dict = parser.parse(dictWithIntsTorrent.toByteArray())
+
+        assert(dict == dictWithIntsRes)
+    }
+
+    @Test
+    fun `dict of dicts`() {
+        val dict = parser.parse(dictOfDictsTorrent.toByteArray())
+
+        assert(dict == dictOfDictsRes)
+    }
+
+    @Test
+    fun `dict with list`() {
+        val dict = parser.parse(dictWithListTorrent.toByteArray())
+
+        assert(dict == dictWithListRes)
+    }
+
+    @Test
+    fun `dict with empty list`() {
+        val dict = parser.parse(dictWithEmptyListTorrent.toByteArray())
+
+        assert(dict == dictWithEmptyListRes)
+    }
 }
